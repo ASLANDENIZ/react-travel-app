8fa375a28f5169093bf087218fed9fa409bb13f1
ASLANDENIZ <dkelemence@gmail.com>
2021-11-24 15:06:06 +0000

    First commit

---
+++ src/components/pages/Discover.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+
+ function Discover() {
+    return (
+        <h1 className="discover">THE WORLD IS YOURS</h1>
+    )
+}
+
+export default Discover;
---
+++ src/components/pages/Earth.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import "../../App.css";
+
+ function Earth() {
+    return (
+        <>
+        
+        <div className="hero-container">
+            <h1>DENIZ KELEMENCE</h1>
+            <video src="/videos/video-2.mp4" autoPlay loop muted></video>
+        </div>
+        </>
+    )
+}
+
+export default Earth
+
+ 
